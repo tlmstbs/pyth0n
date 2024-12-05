a2d6f90471c2c3309bb4322539cf499f27685b72
tlmstbs <viktoriashohova@gmail.com>
2024-12-05 23:58:48 +0400

    Update laba6.py

--- лаба 6/laba6.py
+++ лаба 6/laba6.py
@@ -11,11 +11,12 @@
 На складе находятся компоненты разных компаний. Количество компаний К1, К2, … КN. Сформировать все возможные варианты комплектации компьютеров."""
 
 
-import itertools
-import time
 
-N = 3
-K = [2, 3, 2]
+import itertools
+import timeit
+
+N = 5
+K = [2, 3, 2, 5, 7]
 
 def generate_combinations_algorithmic(K):
     result = [[]]
@@ -49,16 +50,14 @@ def find_best_combination(combinations):
     return best_combination
 
 
-start_time = time.time()
 combinations_algo = generate_combinations_algorithmic(K)
-algo_time = time.time() - start_time
+algo_time = timeit.timeit("generate_combinations_algorithmic(K)",globals=globals(), number=1)
 print("Алгоритмический подход:", combinations_algo)
 print("Время выполнения (алгоритмический):", algo_time)
 
 
-start_time = time.time()
 combinations_python = generate_combinations_python(K)
-python_time = time.time() - start_time
+python_time = timeit.timeit("generate_combinations_python(K)",globals=globals(), number=1)
 print("Python-функции:", combinations_python)
 print("Время выполнения (Python-функции):", python_time)
 
